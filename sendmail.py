b706897623875fa07a81470eabc8d23a6a256093
Yaroslav Chupikov <yaroslav@mirasoltek.com>
2016-01-04 16:23:57 +0200

    Added '--config' argument in purpose of define path to config file. If config file not specified script first lloking for 'sendmail.json' configuration file in the current working directory and if not found then - for 'config.json' file in the script root directory.

--- sendmail.py
+++ sendmail.py
@@ -3,11 +3,24 @@
 import os
 import sys
 import json
-
+import argparse
 from sendmail.sendmail import Sendmail
 
-# filename = os.getcwd() + "/config.json"
-filename = os.path.dirname(os.path.realpath(__file__)) + "/config.json"
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--config", help="Config file name.")
+args = parser.parse_args()
+if args.config:
+    filename = args.config
+else:
+    if os.path.isfile(os.getcwd() + '/sendmail.json'):
+        filename = os.getcwd() + '/sendmail.json'
+    else:
+        filename = os.path.dirname(os.path.realpath(__file__)) + "/config.json"
+
+if not os.path.isfile(filename):
+    sys.stderr.write("ERROR! Config file not found: {}\n".format(filename))
+    sys.exit(1)
+
 f = open(filename, encoding="UTF-8")
 sendmail = Sendmail(json.loads(f.read()))
 f.close()
