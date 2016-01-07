a3b013029ab68a83e815f3fc5ea6540940bb61fa
Yaroslav Chupikov <yaroslav@mirasoltek.com>
2016-01-07 18:02:34 +0200

    Added license and docstrings to modified files. Refs #13

--- sendmail.py
+++ sendmail.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python3
 
+"""Configure and runs Sendmail application.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
 import os
 import sys
 import json
 import argparse
 from sendmail.sendmail import Sendmail
+
+
+__author__ = "Yaroslav Chupikov"
+__copyright__ = "Copyright 2016, Celtic Logic Limited"
+__license__ = "GPL"
+__version__ = "0.3"
+__email__ = "yaroslav@mirasoltek.com"
+__status__ = "Production"
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--config", help="Config file name.")
